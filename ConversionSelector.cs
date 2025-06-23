ec132c09499a604d2c084b265386c82644d98ed4
Parek4Life <125895505+Cherttov@users.noreply.github.com>
2025-06-23 20:41:45 +0200

    Add project files

---
+++ ConversionSelector.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace type_converter
+{
+    public enum ImageFormat
+    {
+        Unknown, // Default 
+        // Picture
+        PNG,
+        JPG,
+        WEBP,
+        // Audio
+        MP3,
+        WAV
+    }
+
+    // Creates conversion table
+    class ConversionSelector
+    {
+        private static readonly Dictionary<ImageFormat, List<ImageFormat>> ConversionMap;
+
+        // Constructor on run
+        static ConversionSelector()
+        {
+            // { PNG : [JPG, WEBP ] } , { JPB : [PNG, WEBP ] }
+            ConversionMap = new Dictionary<ImageFormat, List<ImageFormat>>
+            {
+                // Picture
+                { ImageFormat.PNG, new  List<ImageFormat>{ImageFormat.JPG, ImageFormat.WEBP} },
+                { ImageFormat.JPG, new  List<ImageFormat>{ImageFormat.PNG, ImageFormat.WEBP} },
+                { ImageFormat.WEBP, new  List<ImageFormat>{ImageFormat.JPG, ImageFormat.PNG} },
+                // Audio
+                { ImageFormat.MP3, new  List<ImageFormat>{ImageFormat.WAV} },
+                { ImageFormat.WAV, new  List<ImageFormat>{ImageFormat.MP3} }
+            };
+        }
+        
+        // Returns available conversions for selected format
+        public static List<ImageFormat> GetAllowedConversions (ImageFormat _sourceFormat)
+        {
+            // PNG: ConversionMap.GetValues for PNG -> Output available values
+            if (ConversionMap.TryGetValue(_sourceFormat, out var _convertibleFormats))
+            {
+                Debug.WriteLine($"{_sourceFormat}: {string.Join(", ", _convertibleFormats)}"); // Debug
+                return _convertibleFormats;
+            }
+
+            return new List<ImageFormat>();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Path = System.IO.Path;
+
+namespace type_converter
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ConversionSelector.GetAllowedConversions(ImageFormat.PNG); // Placeholder
+        }
+
+        // Drag & Drop Area
+        private void DragDropArea_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] _files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                // Check if more than 1 file has been selected
+                if (_files.Length > 1)
+                {
+                    Debug.WriteLine("ERROR: MainWindow.xaml.cs : Line 33"); // Debug
+                }
+                else
+                {
+                    string _fileName = Path.GetFileName(_files[0]);
+                    Debug.WriteLine(_fileName); // Debug
+                }
+            }
+        }
+    }
+}
